7b8749daf7d0d6632927598ab8855825542d7566
carlxs-txjada <carlos.tejada5620@soyuco.edu.co>
2023-04-28 09:54:00 -0500

    Mejoras en Utils, nuevo Utils Boolean

--- src/co/edu/uco/publiuco/utils/UtilText.java
+++ src/co/edu/uco/publiuco/utils/UtilText.java
@@ -1,51 +1,69 @@
 package co.edu.uco.publiuco.utils;
 
 public final class UtilText {
-	
+
+	private static final String EMAIL_RE = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+	private static final String DEFAULT_EMAIL = "uconnectrugrats@gmail.com";
+	private static final String NUMERIC_RE = "[0-9]+";
+	private static final String DEFAULT_NUMERIC = "0";
 	private static final UtilText INSTANCE = new UtilText();
 	public static final String EMPTY = "";
-	
+
 	private UtilText() {
 		super();
 	}
-	
+
 	public static final UtilText getUtilText(){
 		return INSTANCE;
 	}
-	
+
 	public static final boolean isNull(final String text) {
 		return UtilObject.isNull(text);
 	}
-	
+
 	public static final String getDefault(final String text, final String defaultValue) {
-		
+
 		String result = text;
 		if (isNull(text)) {
 			result = isNull(defaultValue)? EMPTY: defaultValue;
 		}
 		return result;
 	}
-	
+
 	public static final String getDefaultValue() {
 		return EMPTY;
 	}
-	
+
+	public static final String getDefaultNumeric(){
+		return DEFAULT_NUMERIC;
+	}
+
+	public static final String getDefaultEmailAdress(){
+		return DEFAULT_EMAIL;
+	}
+
 	public static final String getDefault(final String text) {
 		return (getDefault(text, EMPTY));
 	}
-	
+
 	public static final String applyTrim(final String text){
 		return getDefault(text).trim();
 	}
-	
+
 	public final boolean matchPattern(final String text, final String pattern) {
 		return getDefault(text).matches(getDefault(pattern));
 	}
-	
+
+	public static boolean numericIsValid(final String numericString){
+		return isNull(numericString) ? false : getUtilText().matchPattern(numericString, NUMERIC_RE);
+	}
+
+	public static boolean emailIsvalid(final String emailAdress){
+		return isNull(emailAdress) ? false : getUtilText().matchPattern(emailAdress, EMAIL_RE);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("aaaaaaaaaaaaaa");
-		System.out.println(getDefault(null, null));
-		System.out.println(applyTrim("mi casa  "));
+		System.out.println(emailIsvalid("tejadaramirezc@gmail.com"));
 	}
 
 }
