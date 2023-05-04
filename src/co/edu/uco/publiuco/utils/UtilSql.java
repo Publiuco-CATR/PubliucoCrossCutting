4ea1a97204daec5280ea1be4982bdc5790a794da
Carlos Tejada <carlos.tejada5620@soyuco.edu.co>
2023-05-03 21:06:43 -0500

    Q estrees esta mierda de git

--- src/co/edu/uco/publiuco/utils/UtilSql.java
+++ src/co/edu/uco/publiuco/utils/UtilSql.java
@@ -1,43 +1,82 @@
 package co.edu.uco.publiuco.utils;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import co.edu.uco.publiuco.crosscutting.exception.PubliucoCrossCuttingException;
 import co.edu.uco.publiuco.utils.Messages.UtilSqlMessages;
 
-public class UtilSql {
+public final class UtilSql {
 	
-	private 
 
 	private UtilSql() {
 		super();
 	}
 	
-	public static void closeConnection(final Connection connection) {
-		if (!connectionIsOpen(connection)) {
-			try {
-				connection.close();
-			} catch (SQLException e) {
-				
-				e.printStackTrace();
-			}
+	public static final Connection openConnection(final String url, final String username, final String password) {
+		try {
+			Connection connection = DriverManager.getConnection(url, username, password);
+			return connection;
+		} catch (final SQLException exception) {
+			var userMessage = UtilSqlMessages.OPEN_CONNECTION_USER_MESSAGE;
+			var technicalMessage = UtilSqlMessages.OPEN_CONNECTION_TECHNICAL_MESSAGE;
+			
+			throw PubliucoCrossCuttingException.create(technicalMessage, userMessage, exception);
 		}
 	}
 	
-	public static boolean connectionIsOpen(Connection connection) {
+	public static final void closeConnection(final Connection connection) {
+		try {
+			if (!connectionIsOpen(connection)) {
+				connection.close();
+			}
+		} catch (final SQLException exception) {
+			var userMessage = UtilSqlMessages.CONNECTION_IS_OPEN_USER_MESSAGE;
+			var technicalMessage = UtilSqlMessages.CONNECTION_IS_OPEN_TECHNICAL_SQL_EXCEPTION;
+			throw PubliucoCrossCuttingException.create(technicalMessage, userMessage, exception);
+		}
+	}
+	
+	public static boolean connectionIsOpen(final Connection connection) {
 		if (UtilObject.isNull(connection)) {
 			var userMessage = UtilSqlMessages.CONNECTION_IS_OPEN_USER_MESSAGE;
-			var technicalMessage = UtilSqlMessages.CONNECTION_IS_OPEN_TECHNICAL_MESSAGE;
+			var technicalMessage = UtilSqlMessages.CONNECTION_IS_OPEN_TECHNICAL_NULL_CONNECTION;
 			throw PubliucoCrossCuttingException.create(userMessage, technicalMessage);
 		}
 		
 		try {
 			return !connection.isClosed();
-		} catch (final Exception exeption) {
+		} catch (final SQLException exception) {
 			var userMessage = UtilSqlMessages.CONNECTION_IS_OPEN_USER_MESSAGE;
 			var technicalMessage = UtilSqlMessages.CONNECTION_IS_OPEN_TECHNICAL_MESSAGE;
+			throw PubliucoCrossCuttingException.create(technicalMessage, userMessage, exception);
+		} catch (final Exception exeption) {
+			var userMessage = UtilSqlMessages.CONNECTION_IS_OPEN_USER_MESSAGE;
+			var technicalMessage = UtilSqlMessages.CONNECTION_IS_OPEN_TECHNICAL_EXCEPTION;
 			throw PubliucoCrossCuttingException.create(technicalMessage, userMessage, exeption);
 		}
 	}
+	
+	  public static void closeResultSet(ResultSet rs) {
+		    try {
+		      if (rs != null) {
+		        rs.close();
+		      }
+		    } catch (SQLException e) {
+		      // manejo de excepción
+		    }
+		  }
+		  
+		  public static void closePreparedStatement(PreparedStatement ps) {
+		    try {
+		      if (ps != null) {
+		        ps.close();
+		      }
+		    } catch (SQLException e) {
+		      // manejo de excepción
+		    }
+		  }
 }
